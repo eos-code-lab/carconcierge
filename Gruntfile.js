534eb66552e2281b9c2b65ff9d94ebb756a7c511
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-01-02 04:06:53 +0200

    Added less config

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,20 @@ module.exports = function (grunt) {
   require('time-grunt')(grunt);
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      core: {
+        options: {
+          outputSourceFiles: true,
+          sourceMap: true,
+          sourceMapFilename: 'html/assets/app/css/style.css.map',
+          sourceMapURL: 'style.css.map',
+          strictMath: true
+        }
+        files: {
+          'html/assets/app/css/style.css': 'less/style.less'
+        }
+      }
+    }
   });
 };
