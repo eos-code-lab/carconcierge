a634dae2eccb19da6ee5b9bd9ceecd691ceeb166
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-01-02 04:04:14 +0200

    Added grunt plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,9 @@
 module.exports = function (grunt) {
   'use strict';
 
+  require('jit-grunt')(grunt);
+  require('time-grunt')(grunt);
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json')
   });
