293a38aa730659c31b7a58ee2e7dc14483a70321
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-01-03 02:51:04 +0200

    Added main javascript

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 (function ($) {
   'use strict';
 
-  $.noop;
+  var $navbar = $('#navbar');
+  var navbarHeight = $navbar.height();
+
+  $('body').scrollspy({
+    target: '#navbar',
+    offset: navbarHeight + 10
+  });
+
+  $('a[href^="#"]').click(function (event) {
+    event.preventDefault();
+
+    $navbar.collapse('hide');
+
+    var target = this.hash || 'body';
+
+    $(window).stop(true).scrollTo(target, {
+      duration: 1000,
+      interrupt: true,
+      offset: -navbarHeight
+    });
+  });
 })(jQuery);
