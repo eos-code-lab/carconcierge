e4bfb3d0a2946e1dee98acc668da0f8a333e075d
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-01-04 15:53:51 +0200

    Updated main javascript

--- js/main.js
+++ js/main.js
@@ -27,16 +27,16 @@
   var context = {
     reviews: [
       {
-        reviewer: 'Andrei Popescu',
-        avatar: 'assets/app/img/avatar.png',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, officia!',
-        rating: 4
+        reviewer: 'Claudia Ifrim',
+        avatar: 'https://scontent.xx.fbcdn.net/hprofile-xfp1/v/l/t1.0-1/c167.31.386.386/s100x100/44202_1411148200733_735995_n.jpg?oh=ad19b2f805afbf63840d243868f5f190&oe=5744B2AE',
+        text: 'In vara anului trecut am achizitionat o noua masina. Am zis bugetul disponibil si ce as vrea de la masina pe care o caut si Gabi mi-a prezentat 3 oferte, a negociat pretul pentru oferta aleasa, a tinut legatura cu firma de leasing si s-a ocupat de toate formalitatile necesare. Pentru mine a ramas doar un test drive si preluarea masinii livrate.',
+        rating: 5
       },
       {
-        reviewer: 'Ion Popescu',
-        avatar: 'assets/app/img/avatar.png',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, nisi.',
-        rating: 3
+        reviewer: 'Cătălin Dogaru',
+        avatar: 'https://scontent.xx.fbcdn.net/hprofile-xal1/v/t1.0-1/p100x100/11666058_931880383535643_3616477844817336457_n.jpg?oh=e35cb36f719557e423add05bb3275329&oe=56FFF39F',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et dignissimos sapiente iste asperiores pariatur accusamus a voluptatum placeat, cupiditate voluptate.',
+        rating: 4
       }
     ]
   };
