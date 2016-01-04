ee0665de5941edfd932dcdcaf841838a3d63904e
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-01-04 15:01:58 +0200

    Updated main javascript

--- js/main.js
+++ js/main.js
@@ -22,7 +22,29 @@
     });
   });
 
-  $('.testimonials').slick({
+  var source = $('#reviews-template').html();
+  var template = Handlebars.compile(source);
+  var context = {
+    reviews: [
+      {
+        reviewer: 'Andrei Popescu',
+        avatar: 'assets/app/img/avatar.png',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, officia!',
+        rating: 4
+      },
+      {
+        reviewer: 'Ion Popescu',
+        avatar: 'assets/app/img/avatar.png',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, nisi.',
+        rating: 3
+      }
+    ]
+  };
+  var html = template(context);
+
+  $('#reviews-template').after(html);
+
+  $('.reviews').slick({
     autoplay: true,
     nextArrow: '<button type="button" class="slick-next"><span class="glyphicon glyphicon-chevron-right"></span></button>',
     prevArrow: '<button type="button" class="slick-prev"><span class="glyphicon glyphicon-chevron-left"></span></button>'
