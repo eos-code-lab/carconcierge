f2820bc4d162bb96a0df298ffd02f335159c777a
Claudia Ifrim <ifrim.claudia@gmail.com>
2016-04-12 00:08:31 +0300

    update db

--- db/database.sql
+++ db/database.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE carconcierge;
 USE carconcierge;
-DROP TABLE IF EXISTS contact;
-CREATE TABLE contact(
-	id							INT UNSIGNED NOT NULL auto_increment,
-	name						VARCHAR(120) NOT NULL,
-	email						VARCHAR(120) NOT NULL,
-	phone						VARCHAR(120) NOT NULL DEFAULT "",
-	message					VARCHAR(300) NOT NULL,
+DROP TABLE IF EXISTS contacts;
+CREATE TABLE contacts(
+	id					INT UNSIGNED NOT NULL auto_increment,
+	name				VARCHAR(120) NOT NULL,
+	email				VARCHAR(120) NOT NULL,
+	phone				VARCHAR(120) NOT NULL DEFAULT "",
+	message				VARCHAR(300) NOT NULL,
 	added_date			TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	primary key(id)
-)type=innodb DEFAULT CHARACTER SET utf8	COLLATE utf8_general_ci;
+);
