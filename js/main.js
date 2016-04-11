981bb3f5dac41514051290aa3d2a06e3f50c9d37
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-04-11 23:38:08 +0300

    Updated validator defaults

--- js/main.js
+++ js/main.js
@@ -72,20 +72,20 @@
   });
 
   $.validator.setDefaults({
-    highlight: function (element) {
-      $(element).closest('.form-group').addClass('has-error');
-    },
-    unhighlight: function (element) {
-      $(element).closest('.form-group').removeClass('has-error');
-    },
-    errorElement: 'span',
     errorClass: 'help-block',
+    errorElement: 'span',
     errorPlacement: function (error, element) {
       if (element.parent('.input-group').length) {
         error.insertAfter(element.parent());
       } else {
         error.insertAfter(element);
       }
+    },
+    highlight: function (element) {
+      $(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function (element) {
+      $(element).closest('.form-group').removeClass('has-error');
     }
   });
 
