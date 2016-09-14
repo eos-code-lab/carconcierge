cdb381820580e77ab1e6d2917e0ebf3f01126c1a
Claudia Ifrim <ifrim.claudia@gmail.com>
2016-09-14 23:04:22 +0300

    add config template

---
+++ html/config.template.php
@@ -0,0 +1,9 @@
+<?php
+//TODO: Set in config file
+$email_to = 'test.me@mailinator.com';
+$config = array(
+  'database_dsn' => 'mysql:dbname=<your_db_name>;host=<your_host>',
+  'database_user' => '<your_db_user>',
+  'database_password' => '<your_db_pass>',
+  'recaptcha_secret_key' => '<your_recaptcha_secret_key>'
+);
